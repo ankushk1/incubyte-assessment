b778ced7d325459421ffcef4368dc10a62ee7f29
Ankush <ankushkapoor179@gmail.com>
2025-06-09 21:14:05 +0530

    jest configured and added test cases

---
+++ StringCalculator.test.ts
@@ -0,0 +1,30 @@
+import { StringCalculator } from './StringCalculator';
+
+describe("StringCalculator", () => {
+  let calculator: StringCalculator;
+
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
+  test("should return 0 for empty string", () => {
+    expect(calculator.add("")).toBe(0);
+  });
+
+  test("should return the number itself for a single number", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
+
+  test("should return add of two numbers", () => {
+    expect(calculator.add("1,5")).toBe(6);
+  });
+
+  test("should handle multiple numbers", () => {
+    expect(calculator.add("1,2,3,4")).toBe(10);
+  });
+
+  test("should handle newlines as delimiters", () => {
+    expect(calculator.add("1\n2,3")).toBe(6);
+  });
+  
+});
